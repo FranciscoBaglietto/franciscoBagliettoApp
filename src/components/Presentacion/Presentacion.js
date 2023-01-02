3268c0fc8a9c7bb84644bb629c0b24d3781c89df
FranciscoBaglietto <fbagliettopiermattei@gmail.com>
2023-01-02 18:10:24 -0300

    Presentacion1

--- src/components/Presentacion/Presentacion.js
+++ src/components/Presentacion/Presentacion.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import './Presentacion.css'
+import React from "react";
+import "./Presentacion.css";
 
 const Presentacion = () => {
   return (
-    <div>
-   
-    </div>
-  )
-}
+    <div className="presentacion" >
+      <div className="presentacion-titulos">
+        <div className="titulos">
+          <h1 className="titulos-top">Francisco Baglietto</h1>
+          <h3 className="titulos-abajo">Desarrollador FrontEnd</h3>
+        </div>
+        <ul className="presentacion__listas">
+          <li className="presentacion__listas-link"><a className="presentacion__link" href="https://www.w3schools.com">Sobre Mi</a></li>
+          <li className="presentacion__listas-link"><a className="presentacion__link" href="https://www.w3schools.com">Estudios</a></li>
+          <li className="presentacion__listas-link"><a className="presentacion__link" href="https://www.w3schools.com">Proyectos</a></li>
+          <li className="presentacion__listas-link"><a className="presentacion__link" href="https://www.w3schools.com">Contacto</a></li>
+        </ul>
+      </div>
 
-export default Presentacion
+      <div className="presentacion-img">
+    hola
+      </div>
+    </div>
+  );
+};
+
+export default Presentacion;
